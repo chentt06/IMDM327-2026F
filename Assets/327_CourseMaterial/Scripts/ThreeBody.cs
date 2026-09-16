d60e996856fc3b3973216dc8345e5ca7f70b6d6e
Trina Cheng <trinacheng43@gmail.com>
2026-09-16 09:13:10 -0400

    edits

--- Assets/327_CourseMaterial/Scripts/ThreeBody.cs
+++ Assets/327_CourseMaterial/Scripts/ThreeBody.cs
@@ -2,10 +2,17 @@
 // Fall 2026. IMDM 327
 // Instructor. Myungin Lee
 using UnityEngine;
+using System;
+
+
 
 public class ThreeBody : MonoBehaviour
 {
+    private float minimumDistance = 1f;
     private const float G = 500f; // Gravitational constant for this simulation, not the real-world value.
+    public float separationForce = 3f;
+
+    public float fasterTime = 3f;
     BodyProperty[] bp;
     private int numberOfSphere = 3;
     class BodyProperty // why struct?
@@ -31,10 +38,12 @@ public class ThreeBody : MonoBehaviour
 
             // initial conditions
             float r = 100f;
+            float theta = 2 * Mathf.PI /  numberOfSphere * i;
             // position is (x,y,z). In this case, I want to plot them on the circle with r
 
             // ******** Fill in this part ********
-            // bp[i].body.transform.position = new Vector3( ***, *** , 180);
+            bp[i].body.transform.position = new Vector3( r * Mathf.Cos(theta) + UnityEngine.Random.Range(-10f, 10f),
+             r * Mathf.Sin(theta) + UnityEngine.Random.Range(-10f, 10f), 180);
             // z = 180 places the bodies in front of a camera near the origin looking along +Z. Try other positions too.
 
             bp[i].velocity = Vector3.zero; // Try different initial condition
@@ -71,9 +80,66 @@ public class ThreeBody : MonoBehaviour
     {
         // Loop for N-body gravity
         // How should we design the loop?
-        for (int i = 0; i < numberOfSphere; i++)
-        {
-            // Something
+        // for (int i = 0; i < numberOfSphere; i++)
+        // {
+        //     // Something
+        //     bp[i].acceleration = Vector3.zero;
+        //     for(int j = 0; j < numberOfSphere; j++)
+        //     {
+        //         if(j!=i)
+        //         {
+        //             //gravity 
+        //             Vector3 distance = bp[i].body.transform.position - bp[j].body.transform.position;
+        //             Vector3 gravity = CalculateGravity(distance, bp[i].mass, bp[j].mass);
+        //             bp[i].acceleration -= gravity / bp[i].mass;
+
+        //         }
+        //         //acceleration ^
+              
+        //     }
+
+        //     //velocity ^
+
+        //     bp[i].velocity += bp[i].acceleration * Time.deltaTime;
+        //     //position ^
+
+        //     bp[i].body.transform.position += bp[i].velocity * Time.deltaTime;
+        // }
+
+        // so old code was kind of redundant, we can make it more effiecient by considering how
+        // for every outer loop, we have already calcuated the gravity for the first sphere
+        // of every inner loop.
+
+        for(int i = 0; i < numberOfSphere; i++){
+            bp[i].acceleration = Vector3.zero;
+        }
+
+
+
+        for (int i = 0; i < numberOfSphere; i++){
+
+            for(int j = i + 1; j < numberOfSphere; j++)
+            {
+                Vector3 distance = bp[i].body.transform.position - bp[j].body.transform.position;
+                Vector3 gravity = CalculateGravity(distance, bp[i].mass, bp[j].mass);
+                bp[i].acceleration -= gravity / bp[i].mass;
+                bp[j].acceleration += gravity / bp[j].mass;
+
+                if (distance.magnitude < minimumDistance)
+                {
+                    bp[i].acceleration += 3f * gravity / bp[i].mass * fasterTime;
+                    bp[j].acceleration -= 3f * gravity / bp[j].mass * fasterTime;
+                }
+                
+
+            }
+
+         //velocity ^
+
+             bp[i].velocity += bp[i].acceleration * Time.deltaTime;
+        //position ^
+
+             bp[i].body.transform.position += bp[i].velocity * Time.deltaTime;
         }
 
     }
@@ -82,8 +148,9 @@ public class ThreeBody : MonoBehaviour
     private Vector3 CalculateGravity(Vector3 distanceVector, float m1, float m2)
     {
         Vector3 gravity = Vector3.zero; // note this is also Vector3
-                                        // **** Fill in the function below. 
-                                        // gravity = ****;
+        //gravity =
+        gravity = G * m1 * m2 / (distanceVector.sqrMagnitude + 1f) * distanceVector.normalized;       // normalize becomes direction                        // **** Fill in the function below. 
+
         return gravity;
     }
 }
